e6eb78b1b6b009a74dea79673bd6ab4414e30813
VanhouteghemJ <vanhouteghem.j@gmail.com>
2022-12-12 00:13:37 +0100

    clean

--- src/FormFactory/form.tsx
+++ src/FormFactory/form.tsx
@@ -1,22 +1,9 @@
-/* import { useEffect } from "react";
-import { useFormContextProvider } from "./form.context"; */
 import React from "react";
-/* export function FormFactoryComponent(props: any) {
-  // see https://react-hook-form.com/
-  return <>heyhey</>;
-} */
-
-/* const FormFactoryComponent: React.FC<any> = ({ heading, content }: any) => {
-  console.log("haa sans gluten");
-
-  return <div>kikoo</div>;
-}; */
 
 import { useEffect } from "react";
 import { useFormContextProvider } from "./form.context";
 
 export function FormFactoryComponent(props: any) {
-  // make context @input optional
   const defaultContext = useFormContextProvider();
   function getContext() {
     return props.context ? props.context : defaultContext;
@@ -30,18 +17,14 @@ export function FormFactoryComponent(props: any) {
   // todo in hook
   function isDisplay(catalogItem: any): boolean {
     let res = true;
-    // if (props.catalogItem.display &&  props.catalogItem.display() && )
     if (catalogItem.display) {
       res = catalogItem.display(props.context);
     }
     return res;
   }
 
-  // see https://react-hook-form.com/
   return (
     <>
-      {/* <p>Form context data: {JSON.stringify(getContext().data)}</p> */}
-      {/* data.foo || "" => A component is changing an uncontrolled input of type text to be controlled error in ReactJS: https://stackoverflow.com/a/47012342/3086147 */}
       <div>
         {props.context.catalog.map((catalogItem: any, index: number) => (
           <div key={index}>
