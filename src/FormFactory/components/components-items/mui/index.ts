e77f2ef6057f86d784ffaaf300da97771f454e77
VanhouteghemJ <vanhouteghem.j@gmail.com>
2022-12-18 23:47:54 +0100

    doc: add todo

--- src/FormFactory/components/components-items/mui/index.ts
+++ src/FormFactory/components/components-items/mui/index.ts
@@ -3,4 +3,5 @@ import { MuiSwitch } from "./switch";
 import { MuiCheckBox } from "./checkbox";
 import { MuiSelect } from "./select";
 
+// TODO create a readme showing which mui fields are availables
 export { MuiText, MuiSwitch, MuiCheckBox, MuiSelect };
